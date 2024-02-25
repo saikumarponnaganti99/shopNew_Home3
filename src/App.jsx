9098a611338b512391a5c925a149a677d6a8792d
Sai Kumar <saikumarponnaganti99@gmail.com>
2024-02-25 12:33:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import HomeThree from "./components/HomeThree";
+
+function App() {
+  return <HomeThree />;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "react-range-slider-input/dist/style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// AOS.init();
