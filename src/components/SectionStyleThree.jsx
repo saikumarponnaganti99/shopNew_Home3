94a9f9e6b688830ae5d8603cb17fff3fff9c317f
Sai Kumar <saikumarponnaganti99@gmail.com>
2024-02-28 18:21:06 +0530

    Added more six product cards in sectionStylethree file

--- src/components/SectionStyleThree.jsx
+++ src/components/SectionStyleThree.jsx
@@ -14,7 +14,7 @@ export default function SectionStyleThree({
       <ViewMoreTitle categoryTitle={sectionTitle} seeMoreUrl={seeMoreUrl}>
         <div className="products-section w-full">
           <div className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 xl:gap-[30px] gap-5">
-            <DataIteration datas={products} startLength={0} endLength={12}>
+            <DataIteration datas={products} startLength={0} endLength={18}>
               {({ datas }) => (
                 <div data-aos="fade-up" key={datas.id} className="item">
                   <ProductCardStyleOne type={type} datas={datas} />
