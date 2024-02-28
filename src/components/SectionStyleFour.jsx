666ba6a0af99b79f3a69d12bd1ab5802eb0730ac
Sai Kumar <saikumarponnaganti99@gmail.com>
2024-02-28 19:44:11 +0530

    Added 15 extra products in SectionStyleFour file and its responsive

--- src/components/SectionStyleFour.jsx
+++ src/components/SectionStyleFour.jsx
@@ -14,21 +14,21 @@ export default function SectionStyleFour({
         <div className="products-section w-full">
           <div className="grid lg:grid-cols-3 grid-cols-1 xl:gap-[30px] lg:gap-5">
             <div className="item-col">
-              <DataIteration datas={products} startLength={0} endLength={4}>
+              <DataIteration datas={products} startLength={0} endLength={9}>
                 {({ datas }) => (
                   <ProductCardRowStyleTwo key={datas.id} datas={datas} />
                 )}
               </DataIteration>
             </div>
             <div className="item-col">
-              <DataIteration datas={products} startLength={4} endLength={8}>
+              <DataIteration datas={products} startLength={4} endLength={13}>
                 {({ datas }) => (
                   <ProductCardRowStyleTwo key={datas.id} datas={datas} />
                 )}
               </DataIteration>
             </div>
             <div className="item-col">
-              <DataIteration datas={products} startLength={8} endLength={12}>
+              <DataIteration datas={products} startLength={8} endLength={17}>
                 {({ datas }) => (
                   <ProductCardRowStyleTwo key={datas.id} datas={datas} />
                 )}
