10f27b2e5f329ad1ee8fbefb6348ea60cf33f4e2
Sai Kumar <saikumarponnaganti99@gmail.com>
2024-02-29 17:36:04 +0530

    In Navbar section "Become a seller" is placed left side of navbar

--- src/components/Headers/Navbar.jsx
+++ src/components/Headers/Navbar.jsx
@@ -32,6 +32,41 @@ export default function Navbar({ className, type }) {
         <div className="w-full h-full relative">
           <div className="w-full h-full flex justify-between items-center">
             <div className="category-and-nav flex xl:space-x-7 space-x-3 items-center">
+            <div className="become-seller-btn">
+              <a to="/become-saller">
+                <div className="black-btn w-[161px] h-[40px] flex justify-center items-center cursor-pointer">
+                  <div className="flex space-x-2 items-center">
+                    <span className="text-sm font-600">Become a Seller</span>
+                    <span>
+                      <svg
+                        className="fill-current"
+                        width="6"
+                        height="10"
+                        viewBox="0 0 6 10"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <rect
+                          x="1.08984"
+                          width="6.94106"
+                          height="1.54246"
+                          transform="rotate(45 1.08984 0)"
+                          fill="white"
+                        />
+                        <rect
+                          x="6"
+                          y="4.9082"
+                          width="6.94106"
+                          height="1.54246"
+                          transform="rotate(135 6 4.9082)"
+                          fill="white"
+                        />
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              </a>
+            </div>
               <div className="category w-[270px] h-[53px] bg-white px-5 rounded-t-md mt-[6px] relative">
                 <button
                   onClick={handler}
@@ -893,41 +928,7 @@ export default function Navbar({ className, type }) {
                 </ul>
               </div>
             </div>
-            <div className="become-seller-btn">
-              <a to="/become-saller">
-                <div className="black-btn w-[161px] h-[40px] flex justify-center items-center cursor-pointer">
-                  <div className="flex space-x-2 items-center">
-                    <span className="text-sm font-600">Become a Seller</span>
-                    <span>
-                      <svg
-                        className="fill-current"
-                        width="6"
-                        height="10"
-                        viewBox="0 0 6 10"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <rect
-                          x="1.08984"
-                          width="6.94106"
-                          height="1.54246"
-                          transform="rotate(45 1.08984 0)"
-                          fill="white"
-                        />
-                        <rect
-                          x="6"
-                          y="4.9082"
-                          width="6.94106"
-                          height="1.54246"
-                          transform="rotate(135 6 4.9082)"
-                          fill="white"
-                        />
-                      </svg>
-                    </span>
-                  </div>
-                </div>
-              </a>
-            </div>
+            
           </div>
         </div>
       </div>
